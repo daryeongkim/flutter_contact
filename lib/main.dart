2608f9d6157063325e9a2ac775d43bde11189c09
sedy.kim <sedy.kim@kakaoent.com>
2023-01-02 21:57:33 +0900

    앱 디자인 넣기

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,18 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
 
+    return MaterialApp(
+      // 글자위젯 : Text(),
+      // 아이콘위젯 : Icon(Icons.star),
+      // 이미지위젯 : Image.asset('200.png'),
+      // 박스위젯 : Container( width: 50, height: 50, color: Colors.orange )
+      // Center() -> 내 자식 위젯의 기준점을 중앙으로
+      home: Center(
+        child: Container( width: 50, height: 50, color: Colors.orange ),
+      ),
     );
   }
 }
