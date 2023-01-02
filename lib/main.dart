bd0e160b1baa45d211add966a1f731d59817421c
sedy.kim <sedy.kim@kakaoent.com>
2023-01-02 21:37:54 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // app 구동시켜주세요~
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
