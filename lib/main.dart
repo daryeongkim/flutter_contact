b9380efd739255abc31ab261649a11b7ee53f2cb
sedy.kim <sedy.kim@kakaoent.com>
2023-01-09 21:02:58 +0900

    4강 레이아웃 숙제

--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,24 @@ class MyApp extends StatelessWidget {
     // MaterialApp() -> 구글에서 제공하는 위젯을 사용 가능, 커스터마이징 할때도 사용
     // Cupertino어쩌구()-> 아이폰 ui 위젯을 사용 가능
     return MaterialApp(
-      // 글자위젯 : Text(),
-      // 아이콘위젯 : Icon(Icons.star),
-      // 이미지위젯 : Image.asset('200.png'),
-      // 박스위젯 : Container( width: 50, height: 50, color: Colors.orange )
-      // Center() -> 내 자식 위젯의 기준점을 중앙으로
       home: Scaffold(
-       // Scaffold -> 상중하로 나누어주는 위젯
-        body: Column(
-          // body 안에 Row(가로정렬), Column(세로정렬)
-          // MainAxisAlignment.spaceEvenly -> display:flex
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(Icons.star),
-            Icon(Icons.star),
-            Icon(Icons.star)
-          ],
+        appBar: AppBar(
+            title: Text('앱임')
         ),
+        body: Text('안녕'),
+        bottomNavigationBar: BottomAppBar(
+          child: SizedBox(
+            height: 100,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(Icons.phone),
+                  Icon(Icons.message),
+                  Icon(Icons.contact_page),
+                ],
+            ),
+          ),
+        )
       )
     );
   }
