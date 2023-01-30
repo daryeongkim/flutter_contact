a03b8f06a8bd8c25bc59c7f9bc68e2c98882810f
sedy.kim <sedy.kim@kakaoent.com>
2023-01-30 19:58:29 +0900

    8강 커스텀위젯, 숙제

--- lib/main.dart
+++ lib/main.dart
@@ -15,21 +15,52 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: ShopItem(),
+        // ListView : 스크롤바 생김, 스크롤 위치 감지 가능, 메모리 절약
+        body: ListView.builder(
+          itemCount: 100,
+          itemBuilder: (c,i){
+            print(i.toString());
+            return People();
+          },
+        ),
+        bottomNavigationBar: BottomCustom()
       )
     );
   }
 }
 
 // stless
-class ShopItem extends StatelessWidget {
-  const ShopItem({Key? key}) : super(key: key);
+class People extends StatelessWidget {
+  const People({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Text('안뇽'),
+    return ListTile(
+      leading: Icon(Icons.person),
+      title: Text('홍길동'),
     );
   }
 }
+
+class BottomCustom extends StatelessWidget {
+  const BottomCustom({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: SizedBox(
+        height: 100,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.phone),
+            Icon(Icons.message),
+            Icon(Icons.contact_page),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
